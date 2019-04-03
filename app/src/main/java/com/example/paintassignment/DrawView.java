f06255e60357690abbf3f9e2a141f282ad7757e3
rahul <rrahulrajappan@gmail.com>
2019-04-03 18:24:19 +1300

    version 1

--- app/src/main/java/com/example/paintassignment/DrawView.java
+++ app/src/main/java/com/example/paintassignment/DrawView.java
@@ -1,6 +1,20 @@
 package com.example.paintassignment;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class DrawView extends View {
+
+    public DrawView(Context context) {
+        super(context);
+    }
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
