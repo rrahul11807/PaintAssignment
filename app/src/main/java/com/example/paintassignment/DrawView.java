f2e5ae0537b6680ce197302ace358e9974137834
rahul <rrahulrajappan@gmail.com>
2019-04-09 15:06:30 +1200

    version 1

--- app/src/main/java/com/example/paintassignment/DrawView.java
+++ app/src/main/java/com/example/paintassignment/DrawView.java
@@ -1,20 +1,82 @@
 package com.example.paintassignment;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class DrawView extends View {
+import java.util.ArrayList;
+
+public class DrawView extends View implements View.OnTouchListener{
+
+   class PaintCoordinate
+   {
+       PointF pt;
+       float radius;
+   }
+
+   ArrayList <PaintCoordinate> points = new ArrayList<PaintCoordinate>();
+
+   public float getRadius()
+   {
+       return radius;
+   }
+
+   public void setRadius(float radius)
+   {
+       this.radius = radius;
+   }
+   float radius = 3;
 
     public DrawView(Context context) {
         super(context);
+        setup();
     }
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setup();
     }
 
     public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setup();
     }
+
+    public void setup()
+    {
+        setOnTouchListener(this);
+    }
+    @Override
+    protected void onDraw(Canvas canvas){
+        Paint paint = new Paint();
+
+        paint.setColor(Color.RED);
+
+        for(PaintCoordinate pc : points)
+        {
+            canvas.drawCircle(pc.pt.x, pc.pt.y, pc.radius, paint);
+        }
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent)
+    {
+        PaintCoordinate pc = new PaintCoordinate();
+        PointF pt = new PointF();
+        pt.set( motionEvent.getX(),motionEvent.getY());
+
+        pc.pt = pt;
+        pc.radius = radius;
+
+        points.add(pc);
+
+        invalidate();
+
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/paintassignment/MainActivity.java
+++ app/src/main/java/com/example/paintassignment/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.paintassignment;
 
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawView dv = findViewById(R.id.View);
+        dv.setRadius(5);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                dv.setRadius(i);
+                 dv.invalidate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
